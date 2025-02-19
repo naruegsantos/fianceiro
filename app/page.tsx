f3e5db416a0fb630dd1102f7a7d5348e38627df9
naruesantos <naruesantos@gmail.com>
2025-02-19 10:51:58 -0300

    :lipstick:feat atualizei a UI da página inicial e adicionei o shadcn para componentes pré estilizados

--- app/page.tsx
+++ app/page.tsx
@@ -14,36 +14,38 @@ export default function Home() {
       <header className="shadow-md bg-dark border-b-4 border-dotted h-80 flex flex-col items-center justify-center">
         <div className=" text-5xl md:text-6xl drop-shadow-lg title ease-in flex w-fit gap-[-5rem]">
           <h1 className="mr-[-1.5rem]">Fianceiro</h1>
-          <span className="rotate-[20deg]"><FaRedhat style={{color:"var(--hightlight)"}}/></span>
+          <span className="rotate-[20deg]"><FaRedhat style={{ color: "var(--hightlight)" }} /></span>
         </div>
         <span className="text-light text-lg drop-shadow-sm ">contabilidade simples e certeira</span>
-      </header> 
+      </header>
       <main className="flex flex-col items-center">
         <div className="w-full flex flex-col gap-5 justify-center items-center h-[20rem] mb-5 text-center">
           <h1 className="text-2xl md:text-5xl">
             Não subestime a tarefa de ter uma vida financeira <strong className="text-light">saudável!</strong>
           </h1>
-          <h2 className="md:text-4xl border-[0.4rem] border-dotted border-hightlight p-2 px-6 shadow-xl shadow-shadow  w-fit">Ajudamos você com isso</h2>
+          <Link href={"/signup"} className="md:text-4xl border-[0.4rem]  hover:scale-110 rounded-[1rem] hover:underline decoration-blue-500 transition border-dotted border-hightlight py-6 px-10 shadow-xl shadow-shadow  w-fit">
+            Ajudamos você com isso
+          </Link>
         </div>
         <section className="flex justify-center md:justify-center mb-40 gap-20 flex-wrap w-[90%]">
           <Link href={"/consultoria"} className="w-44  hover:scale-110 hover:hue-rotate-180 hover:bg-light h-44 flex flex-col justify-center items-center rounded-3xl font-bold text-xl bg-white transition text-black">
-            <FaBrain style={{fontSize:"2rem"}}/>
+            <FaBrain style={{ fontSize: "2rem" }} />
             <span>consultoria</span>
           </Link>
           <Link href={"/plataforma"} className="w-44 h-44 cursor-pointer text-center hover:scale-110 hover:hue-rotate-30 self-start  flex flex-col justify-center items-center rounded-3xl font-bold text-xl bg-purple-500 transition text-white">
-            <FaLaptopCode style={{fontSize:"2rem"}}/>
+            <FaLaptopCode style={{ fontSize: "2rem" }} />
             <span>plataforma profissional</span>
           </Link>
           <Link href={"/serviço"} className="w-44 h-44 text-center hover:scale-110 hover:hue-rotate-30 self-start  flex flex-col justify-center items-center rounded-3xl font-bold text-xl bg-dark transition text-hightlight">
-            <IoIosPerson style={{fontSize:"2rem"}}/>
+            <IoIosPerson style={{ fontSize: "2rem" }} />
             <span>serviço contábil</span>
           </Link>
-          
+
         </section>
-        <Cards liabilities={liabilitiesMock} assets={assetsMock}/>
+        <Cards liabilities={liabilitiesMock} assets={assetsMock} />
         <section className=" text-center drop-shadow-md dark:text-black shadow-black  bg-white m-20 py-20 shadow-inner w-full p-4" >
-          <h1 className=" text-center text-5xl md:text-6xl">Tenha a sua vida finanecira nas suas <strong className="drop-shadow-lg text-hightlight">mãos!</strong>  </h1>
-          <Link href={"/signup"} className="underline decoration-dashed decoration-[2px] text-xl decoration-hightlight">Junte-se a nós</Link>
+          <h1 className=" text-center text-black text-5xl md:text-6xl">Tenha a sua vida finanecira nas suas <strong className="drop-shadow-lg text-hightlight">mãos!</strong>  </h1>
+          <Link href={"/signup"} className="underline text-black  decoration-dashed decoration-[2px] text-2xl decoration-hightlight">Junte-se a nós</Link>
         </section>
       </main>
       <footer className="dark:bg-dark text-white h-[10vh]">
